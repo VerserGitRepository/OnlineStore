7b964b57647f1c0bdb82f03e8ffe163f2d199b40
VerserGitRepository <basanagouda.patil@verser.com.au>
2020-09-14 10:17:13 +1000

    New changes

---
+++ OnlineStore.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using OnlineStore.WebUI.Infrastructure.HelperServices;
+using OnlineStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            Session["Username"] = null;
+            Session["FullName"] = null;
+            Session["ErrorMessage"] = null;
+            Session.Clear();
+            Session.RemoveAll();
+            Session.Abandon();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel login)
+        {
+            if (string.IsNullOrEmpty(login.UserName) || string.IsNullOrEmpty(login.Password))
+            {
+                Session.Clear();
+                Session.RemoveAll();
+                Session.Abandon();
+                Session["ErrorMessage"] = "Username Or Password Is Empty!";
+                return View("Login");
+            }
+            LoginModel user = new LoginModel { UserName = login.UserName, Password = login.Password };
+            Task<LoginModel> userReturn = LoginService.Login(user);
+            if (userReturn.Result.IsLoggedIn == true)
+            {
+                Session["Username"] = login.UserName;
+                Session["FullName"] = userReturn.Result.FullName;
+                Session["ErrorMessage"] = null;
+                return RedirectToAction("Index", "OrdersList");
+            }
+            else
+            {
+                Session["ErrorMessage"] = "Invalid UserName Or Password";
+                Session.Clear();
+                Session.RemoveAll();
+                return View("Login");
+            }
+        }
+        public ActionResult Logout(LoginModel login)
+        {
+            Session["Username"] = null;
+            Session["FullName"] = null;
+            Session["ErrorMessage"] = null;
+            Session.Clear();
+            Session.RemoveAll();
+            Session.Abandon();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ OnlineStore.WebUI/Infrastructure/HelperServices/LoginService.cs
@@ -0,0 +1,52 @@
+﻿using OnlineStore.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OnlineStore.WebUI.Infrastructure.HelperServices
+{
+    public class LoginService
+    {
+        public static readonly string BaseUri = ConfigurationManager.AppSettings["LoginManagerBase"];
+        public async static Task<LoginModel> Login(LoginModel login)
+        {
+            LoginModel returnmessage = new LoginModel();
+
+            using (HttpClient client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                HttpResponseMessage response = client.PostAsJsonAsync("Login/AuthenticateUser", login).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<LoginModel>();
+                    returnmessage = result;
+                }
+            }
+            return returnmessage;
+        }
+        public async static Task<List<ListItems>> UserRoleList(string UserName)
+        {
+            var returnmessage = new List<ListItems>();
+
+            using (HttpClient client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                HttpResponseMessage response = client.GetAsync(string.Format("Login/{0}/UserRole", UserName)).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<ListItems>>();
+                    foreach (var p in result)
+                    {
+                        returnmessage.Add(new ListItems() { ID = p.ID, Value = p.Value });
+                    }
+                }
+            }
+            return returnmessage;
+        }
+    }
+}
