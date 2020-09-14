56cc4f60563cb28ae14121f80497cc886a3c0866
VerserGitRepository <basanagouda.patil@verser.com.au>
2020-09-14 11:40:01 +1000

    Login and Logout Changes

--- OnlineStore.WebUI/Controllers/LoginController.cs
+++ OnlineStore.WebUI/Controllers/LoginController.cs
@@ -1,17 +1,12 @@
 ﻿using OnlineStore.WebUI.Infrastructure.HelperServices;
 using OnlineStore.WebUI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Mvc;
 
 namespace OnlineStore.WebUI.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
         public ActionResult Index()
         {
             return View();
