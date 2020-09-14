1e811c40e3eb8384a4403f4c72e3c5fbf563c9eb
VerserGitRepository <basanagouda.patil@verser.com.au>
2020-09-14 11:50:15 +1000

    Create ManualOrdersViewModel.cs

---
+++ OnlineStore.WebUI/Models/ManualOrdersViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.WebUI.Models
+{
+    public class ManualOrdersViewModel
+    {
+        public int Id { get; set; }       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }      
+        public string Salutation { get; set; }
+        public string Locality { get; set; }
+        public string Postcode { get; set; }
+        public string State { get; set; }
+        public string SKU { get; set; }
+        public int? JOBID { get; set; }
+        public int? ContactNumber { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public string OrderStatus { get; set; }
+        public string OrderType { get; set; }
+        public DateTime? OrderUpdateDate { get; set; }
+        public string RefNo { get; set; }
+        public bool? IsExport { get; set; }
+        public string Comments { get; set; }
+        public SelectList CustomerProject { get; set; }
+    }
+}
