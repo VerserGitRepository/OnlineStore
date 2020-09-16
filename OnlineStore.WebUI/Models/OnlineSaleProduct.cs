09bf3aa27d713ddda0aaab25fd3cd48de680ab9e
VerserGitRepository <basanagouda.patil@verser.com.au>
2020-09-16 15:00:16 +1000

    Create OnlineSaleProduct.cs

---
+++ OnlineStore.WebUI/Models/OnlineSaleProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineStore.WebUI.Models
+{
+    public class OnlineSaleProduct
+    {
+        public int Id { get; set; }
+        public byte[] ProductImage { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public bool IsSKU { get; set; }
+        public int? ItemType_ID { get; set; }
+        public int? Brand_ID { get; set; }
+        public int? Model_ID { get; set; }
+        public decimal PriceExGST { get; set; }
+        public decimal GSTAmount { get; set; }
+        public decimal PriceIncGST { get; set; }
+        public int QtyAvailable { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
