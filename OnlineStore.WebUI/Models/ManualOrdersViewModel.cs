a54adcf0fb9abb6b1d2d874bef93c4e6e25d0c51
Kalyan Vedula <kalyan.vedula@verser.com.au>
2020-10-12 12:05:50 +1100

    validation changes, ui alignment

--- OnlineStore.WebUI/Models/ManualOrdersViewModel.cs
+++ OnlineStore.WebUI/Models/ManualOrdersViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,17 +12,28 @@ namespace OnlineStore.WebUI.Models
         public ManualOrdersViewModel()
         {
         }
-        public int Id { get; set; }       
+        public int Id { get; set; }
+        [Required(ErrorMessage = "FirstName Is Mandatory")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Surname Is Mandatory")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "AddressLine1 Is Mandatory")]
         public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }      
+        [Required(ErrorMessage = "AddressLine2 Is Mandatory")]
+        public string AddressLine2 { get; set; }
+        [Required(ErrorMessage = "Saluation Is Mandatory")]
         public string Salutation { get; set; }
+        [Required(ErrorMessage = "Locality Is Mandatory")]
         public string Locality { get; set; }
+        [Required(ErrorMessage = "Postcode Is Mandatory")]
         public string Postcode { get; set; }
+        [Required(ErrorMessage = "State Is Mandatory")]
         public string State { get; set; }
+        [Required(ErrorMessage = "Email Is Mandatory")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "ContactNumber Is Mandatory")]
         public int? ContactNumber { get; set; }
+        [Required(ErrorMessage = "SKU Is Mandatory")]
         public string SKU { get; set; }
         public int? JOBID { get; set; }       
         public DateTime? OrderDate { get; set; }
@@ -33,6 +45,7 @@ namespace OnlineStore.WebUI.Models
         public string Comments { get; set; }
         public SelectList CustomerProject { get; set; }
         public int? ProjectId { get; set; }
+        [Required(ErrorMessage = "SSN Is Mandatory")]
         public string SSN { get; set; }
         public string Height { get; set; }
         public string Weight { get; set; }
--- OnlineStore.WebUI/Models/OnlineSaleProduct.cs
+++ OnlineStore.WebUI/Models/OnlineSaleProduct.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,17 +14,24 @@ namespace OnlineStore.WebUI.Models
             Images = new List<string>();
         }
         public int Id { get; set; }
+        [Required(ErrorMessage = "ProductImage Is Mandatory")]
         public byte[] ProductImage { get; set; }
+        [Required(ErrorMessage = "ProductName Is Mandatory")]
         public string ProductName { get; set; }
         public string Description { get; set; }
-        public bool IsSKU { get; set; }       
+        public bool IsSKU { get; set; }
+        [Required(ErrorMessage = "ItemType Is Mandatory")]
         public int? ItemType_ID { get; set; }
+        [Required(ErrorMessage = "Brand Is Mandatory")]
         public int? Brand_ID { get; set; }
+        [Required(ErrorMessage = "Model Is Mandatory")]
         public int? Model_ID { get; set; }
         public int PurchasedQty { get; set; }
         public decimal PriceExGST { get; set; }
         public decimal GSTAmount { get; set; }
+        [Required(ErrorMessage = "Price Is Mandatory")]
         public decimal PriceIncGST { get; set; }
+        [Required(ErrorMessage = "Quantity Is Mandatory")]
         public int QtyAvailable { get; set; }
         public string CreatedBy { get; set; }
         public string ItemTypeName { get; set; }
@@ -35,7 +43,8 @@ namespace OnlineStore.WebUI.Models
         public SelectList Sizes { get; set; }
         public string SKU { get; set; }
         public SelectList Models { get; set; }
-        public List<string> Images { get; set; }      
+        public List<string> Images { get; set; }
+        [Required(ErrorMessage = "ProductImage Is Mandatory")]
         public HttpPostedFileBase[] files { get; set; }
         public bool IsProductActive { get; set; }
         public bool IsMainPageProduct { get; set; }
