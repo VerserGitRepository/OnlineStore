19197e639e1a42cbfd4c96921050ee9ca2262872
VerserGitRepository <basanagouda.patil@verser.com.au>
2020-12-17 10:27:40 +1100

    Create AssetAuctionController.cs

---
+++ OnlineStore.WebUI/Controllers/AssetAuctionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineStore.WebUI.Controllers
+{
+    public class AssetAuctionController : Controller
+    {
+        // GET: AssetAuction
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AuctionAssetBundleBidingDetails()
+        {
+            return View();
+        }
+
+
+    }
+}
