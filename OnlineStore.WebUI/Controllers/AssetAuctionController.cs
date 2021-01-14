08b10119174c3cb39463b32495872b24e53168ed
kalyan <kalyan.vedula@verser.com.au>
2021-01-14 14:46:14 +1100

    excel export

--- OnlineStore.WebUI/Controllers/AssetAuctionController.cs
+++ OnlineStore.WebUI/Controllers/AssetAuctionController.cs
@@ -2,9 +2,12 @@
 using OnlineStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace OnlineStore.WebUI.Controllers
 {
@@ -34,6 +37,37 @@ namespace OnlineStore.WebUI.Controllers
         {
             var assets = OrdersServices.AuctionAssetList(BundleId).Result;
             return PartialView("_AuctionAssetList",assets);
+        }
+        [HttpPost]
+        public ActionResult ExportAuctionResult(int BundleId)
+        {
+            var assets = OrdersServices.AuctionAssetList(BundleId).Result;
+            GridView gv = new GridView();
+            gv.DataSource = assets;
+
+            gv.DataBind();
+            foreach (GridViewRow row in gv.Rows)
+            {
+                for (int i = 0; i < row.Cells.Count; i++)
+                {
+                    if (row.RowType == DataControlRowType.DataRow)
+                    {
+                        row.Cells[i].Attributes.Add("class", "text");
+                    }
+                }
+            }
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=AssetsList.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.Charset = "";
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new System.Web.UI.HtmlTextWriter(sw);
+            gv.RenderControl(htw);
+            Response.Output.Write(sw.ToString());
+            Response.Flush();
+            Response.End();
+            return RedirectToAction("Index", "AssetAuction");
         }
     }
 }
